21c429411e344a216770aa3647fa25a2b4e214ee
Tristan Duenas <tristanduenas@gmail.com>
2016-01-17 11:59:08 -0500

    Sorts with function ptr compar

--- IO.h
+++ IO.h
@@ -1,7 +1,7 @@
 #ifndef IO_H
 #define IO_H
 
-int read_data(int** data, int* out_count);
-int write_data(int** data, int count);
+int read_data(int** data, int* out_count, char* file_name);
+int write_data(int** data, int count, char* file_name);
 
 #endif
